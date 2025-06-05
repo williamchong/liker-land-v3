07ccd002a2f802eae06a8cf1289e92c85980b2a5
William Chong <6198816+williamchong@users.noreply.github.com>
2025-06-05 18:29:10 +0800

    💩 Force pages to use lower case NFT class ID (#33)

--- pages/reader.vue
+++ pages/reader.vue
@@ -16,10 +16,23 @@ definePageMeta({
   },
 })
 
+const route = useRoute()
+const localeRoute = useLocaleRoute()
+const getRouteBaseName = useRouteBaseName()
 const { t: $t } = useI18n()
 
 const nftClassId = computed(() => getRouteQuery('nft_class_id'))
 const bookInfo = useBookInfo({ nftClassId: nftClassId.value })
+
+if (nftClassId.value !== nftClassId.value.toLowerCase()) {
+  await navigateTo(localeRoute({
+    name: getRouteBaseName(route),
+    query: {
+      ...route.query,
+      nft_class_id: nftClassId.value.toLowerCase(),
+    },
+  }), { replace: true })
+}
 
 useHead({
   titleTemplate: () => {
