1a559155b1b24446580c297dc1e5315592081107
William Chong <me@williamchong.cloud>
2025-06-27 02:09:08 +0800

    ✨ Implement pdf reader

--- plugins/polyfill.ts
+++ plugins/polyfill.ts
@@ -3,3 +3,18 @@ if (!Object.hasOwn) {
     return Object.prototype.hasOwnProperty.call(obj, prop)
   }
 }
+
+if (!Promise.withResolvers) {
+  Promise.withResolvers = function<T>() {
+    let resolve!: (value: T | PromiseLike<T>) => void
+    let reject!: (reason?: unknown) => void
+    const promise = new Promise<T>((res, rej) => {
+      resolve = res
+      reject = rej
+    })
+    return { promise, resolve, reject }
+  }
+}
+
+export default defineNuxtPlugin(() => {
+})
