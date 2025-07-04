988ace1fdd4c36edf445c5c12ab890868af5bc20
William Chong <me@williamchong.cloud>
2025-07-04 15:25:04 +0800

    ⚡️ Round /products API ts to minute

--- stores/bookstore.ts
+++ stores/bookstore.ts
@@ -43,7 +43,7 @@ export const useBookstoreStore = defineStore('bookstore', () => {
         isFetching: false,
         hasFetched: false,
         offset: undefined,
-        ts: Date.now(),
+        ts: Math.floor(Date.now() / 60000) * 60000, // Round to the nearest minute
       }
     }
     try {
