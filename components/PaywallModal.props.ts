9aa315b6c3a2ab47e1b61518f213359aa38a091d
William Chong <me@williamchong.cloud>
2025-07-14 02:49:49 +0800

    📈 Allow defining UTM in openPaywallModal

--- components/PaywallModal.props.ts
+++ components/PaywallModal.props.ts
@@ -8,8 +8,15 @@ export interface PaywallModalProps {
   'discountedYearlyPrice'?: string | number
   'discountedMonthlyPrice'?: string | number
   'isProcessingSubscription'?: boolean
+  'utmCampaign'?: string
+  'utmMedium'?: string
+  'utmSource'?: string
   'onUpdate:modelValue'?: (value: SubscriptionPlan) => void
-  'onSubscribe'?: () => void
+  'onSubscribe'?: (props: {
+    utmCampaign?: string
+    utmMedium?: string
+    utmSource?: string
+  }) => void
   'onOpen'?: () => void
   'onClose'?: () => void
 }
