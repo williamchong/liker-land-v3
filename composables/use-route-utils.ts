6bfa9a52ad2265e71f2f68527b0c31860488a76f
Ng Wing Tat, David <i@ngwingt.at>
2025-07-17 09:47:12 +0800

    🐛 Properly place useRoute() for route utils (#201)

--- composables/use-route-utils.ts
+++ composables/use-route-utils.ts
@@ -1,14 +1,14 @@
 export function useRouteQuery() {
+  const route = useRoute()
   return function getRouteQuery(key: string, defaultValue = '') {
-    const route = useRoute()
     const value = route.query[key]
     return (Array.isArray(value) ? value[0] : value) || defaultValue
   }
 }
 
 export function useRouteParam() {
+  const route = useRoute()
   return function getRouteParam(key: string, defaultValue = '') {
-    const route = useRoute()
     const value = route.params[key]
     return (Array.isArray(value) ? value[0] : value) || defaultValue
   }
