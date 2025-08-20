964914650850e9792e5a467ff3e30f7215280612
Ng Wing Tat, David <i@ngwingt.at>
2025-08-20 17:29:46 +0800

    💄 Add Brave & WalletConnect icon

--- components/LoginConnectorIcon.vue
+++ components/LoginConnectorIcon.vue
@@ -7,10 +7,12 @@
 </template>
 
 <script setup lang="ts">
+import brave from '~/assets/images/login-connectors/brave.svg?url'
 import cosmostation from '~/assets/images/login-connectors/cosmostation.svg?url'
 import keplr from '~/assets/images/login-connectors/keplr.svg?url'
 import metamask from '~/assets/images/login-connectors/metamask.svg?url'
 import rabby from '~/assets/images/login-connectors/rabby.svg?url'
+import walletConnect from '~/assets/images/login-connectors/walletconnect.svg?url'
 
 const props = defineProps<{
   connectorId: string
@@ -18,6 +20,8 @@ const props = defineProps<{
 
 const src = computed(() => {
   switch (props.connectorId) {
+    case 'com.brave.wallet':
+      return brave
     case 'io.cosmostation':
       return cosmostation
     case 'app.keplr':
@@ -26,6 +30,8 @@ const src = computed(() => {
       return metamask
     case 'io.rabby':
       return rabby
+    case 'walletConnect':
+      return walletConnect
     default:
       return ''
   }
