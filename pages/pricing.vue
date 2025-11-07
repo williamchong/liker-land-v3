8e2a996a285f203e50d4ae20e07dfd76ad3b0db1
Ng Wing Tat, David <i@ngwingt.at>
2025-11-07 15:43:33 +0800

    🐛 Include query string for /pricing redirect

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -1,7 +1,8 @@
 <script setup>
+const route = useRoute()
 const localeRoute = useLocaleRoute()
 
-await navigateTo(localeRoute({ name: 'member' }), {
+await navigateTo(localeRoute({ name: 'member', query: route.query }), {
   redirectCode: 301,
   external: false,
 })
