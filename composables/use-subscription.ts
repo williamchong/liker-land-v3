918ebcda6623976f24cca4fc9ff68683bcd0c9a3
William Chong <me@williamchong.cloud>
2025-12-19 04:50:42 +0800

    ✨ Support multi-currency price display

--- composables/use-subscription.ts
+++ composables/use-subscription.ts
@@ -238,6 +238,7 @@ export function useSubscription() {
     likerPlusPeriod,
     getPlusDiscountPrice,
     getPlusDiscountRate,
+    PLUS_BOOK_PURCHASE_DISCOUNT,
     isProcessingSubscription,
 
     openPaywallModal,
