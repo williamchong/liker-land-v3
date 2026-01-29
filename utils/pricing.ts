dac6baf0e940201f01d7c6982cc8a5be5f5c1c45
William Chong <me@williamchong.cloud>
2026-01-29 16:18:38 +0800

    🥅 Use max values for conversion rate if price is > pricing table

--- utils/pricing.ts
+++ utils/pricing.ts
@@ -2,13 +2,26 @@ import { USD_PRICE_TIER_LIST, HKD_PRICE_TIER_LIST, TWD_PRICE_TIER_LIST } from '~
 
 export type PricingCurrency = 'hkd' | 'twd' | 'usd'
 
+const MAX_USD = USD_PRICE_TIER_LIST[USD_PRICE_TIER_LIST.length - 1]!
+const MAX_HKD = HKD_PRICE_TIER_LIST[HKD_PRICE_TIER_LIST.length - 1]!
+const MAX_TWD = TWD_PRICE_TIER_LIST[TWD_PRICE_TIER_LIST.length - 1]!
+
 export function convertUSDPriceToCurrency(price: number, currency: PricingCurrency): number {
+  if (price <= 0) {
+    return 0
+  }
   switch (currency) {
     case 'hkd': {
+      if (price > MAX_USD) {
+        return Math.floor(price * (MAX_HKD / MAX_USD))
+      }
       const index = Math.min(Math.round(price), HKD_PRICE_TIER_LIST.length - 1)
       return HKD_PRICE_TIER_LIST[index] ?? 0
     }
     case 'twd': {
+      if (price > MAX_USD) {
+        return Math.floor(price * (MAX_TWD / MAX_USD))
+      }
       const index = Math.min(Math.round(price), TWD_PRICE_TIER_LIST.length - 1)
       return TWD_PRICE_TIER_LIST[index] ?? 0
     }
@@ -21,14 +34,18 @@ export function convertUSDPriceToCurrency(price: number, currency: PricingCurren
 export function convertCurrencyToUSDPrice(price: number, currency: PricingCurrency): number {
   switch (currency) {
     case 'hkd': {
+      if (price > MAX_HKD) {
+        return Math.floor(price * (MAX_USD / MAX_HKD))
+      }
       const index = HKD_PRICE_TIER_LIST.findIndex(tierPrice => tierPrice >= price)
-      const maxPrice = USD_PRICE_TIER_LIST[USD_PRICE_TIER_LIST.length - 1] ?? 100.99
-      return index >= 0 ? (USD_PRICE_TIER_LIST[index] ?? maxPrice) : maxPrice
+      return index >= 0 ? (USD_PRICE_TIER_LIST[index] ?? MAX_USD) : MAX_USD
     }
     case 'twd': {
+      if (price > MAX_TWD) {
+        return Math.floor(price * (MAX_USD / MAX_TWD))
+      }
       const index = TWD_PRICE_TIER_LIST.findIndex(tierPrice => tierPrice >= price)
-      const maxPrice = USD_PRICE_TIER_LIST[USD_PRICE_TIER_LIST.length - 1] ?? 100.99
-      return index >= 0 ? (USD_PRICE_TIER_LIST[index] ?? maxPrice) : maxPrice
+      return index >= 0 ? (USD_PRICE_TIER_LIST[index] ?? MAX_USD) : MAX_USD
     }
     case 'usd':
     default:
