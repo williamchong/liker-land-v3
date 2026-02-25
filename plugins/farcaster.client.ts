e53749bf005ac17d48d911af5dc63bb153666f1d
William Chong <me@williamchong.cloud>
2026-02-25 17:28:57 +0800

    🐛 Dont init farcaster in 3ook App

--- plugins/farcaster.client.ts
+++ plugins/farcaster.client.ts
@@ -1,7 +1,8 @@
 export default defineNuxtPlugin(async () => {
+  const { isApp } = useAppDetection()
   // Don't load mini app SDK if not in a mini app environment
   // https://github.com/farcasterxyz/miniapps/blob/9edfb783f40ce1b2504bfdf2e2f2e15815541771/packages/miniapp-sdk/src/sdk.ts#L35
-  if (import.meta.client && window && (!!window.ReactNativeWebView || window !== window.parent)) {
+  if (import.meta.client && window && !isApp && (!!window.ReactNativeWebView || window !== window.parent)) {
     try {
       const { sdk: farcasterSdk } = await import('@farcaster/miniapp-sdk')
       await farcasterSdk.actions.ready()
