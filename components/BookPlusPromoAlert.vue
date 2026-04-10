43ed65aa0994e6f13cd023cfaf9b8c563dbb584b
Ng Wing Tat, David <i@ngwingt.at>
2026-04-10 11:29:26 +0800

    💄 Update Plus promo banner

--- components/BookPlusPromoAlert.vue
+++ components/BookPlusPromoAlert.vue
@@ -3,12 +3,11 @@
     :title="$t('product_page_plus_promo_title')"
     :description="$t('product_page_plus_promo_description')"
     icon="i-material-symbols-celebration-outline-rounded"
-    color="primary"
+    color="neutral"
     variant="subtle"
     orientation="horizontal"
     :ui="{
-      root: 'rounded-lg items-center gap-3 py-2 px-3',
-      wrapper: 'gap-0.5',
+      root: 'rounded-xl items-center gap-3',
       title: 'text-sm font-bold',
       description: 'text-xs',
       icon: 'size-5',
