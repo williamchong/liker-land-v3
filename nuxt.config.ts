72687168eff9217974423db7b422c5361c895523
William Chong <me@williamchong.cloud>
2026-07-06 19:35:57 +0800

    ⏪️ Revert "✨ Re-add Microsoft UET (Bing Ads) tracking on mainnet"
    This reverts commit 38a793de71a9491b739ba59e60a4a4a2d447c6aa.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -72,9 +72,6 @@ export default defineNuxtConfig({
     uploadedBooksBucketPrefix: process.env.UPLOADED_BOOKS_BUCKET_PREFIX,
     public: {
       scripts: {
-        bingUet: {
-          id: '',
-        },
         googleAnalytics: {
           id: process.env.GA_TRACKING_ID || '',
         },
@@ -366,20 +363,13 @@ export default defineNuxtConfig({
   scripts: {
     privacy: false,
     registry: {
-      bingUet: {
-        bundle: false,
-        proxy: false,
-        trigger: 'onNuxtReady',
-      },
       googleAnalytics: {
         bundle: false,
         proxy: false,
-        trigger: 'onNuxtReady',
       },
       metaPixel: {
         bundle: false,
         proxy: false,
-        trigger: 'onNuxtReady',
       },
       posthog: POSTHOG_PUBLIC_KEY
         ? {
@@ -438,8 +428,6 @@ export default defineNuxtConfig({
           'https://static.intercomassets.com',
           'https://explorer-api.walletconnect.com',
           'https://i.ytimg.com',
-          'https://bat.bing.com',
-          'https://bat.bing.net',
           ...[
             process.env.LIKECOIN_API_ENDPOINT,
             process.env.LIKECOIN_STATIC_ENDPOINT,
