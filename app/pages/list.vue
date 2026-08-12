62c36f25fe6d4b850a51ca6f0c39f7ad0b9868e8
Ng Wing Tat, David <i@ngwingt.at>
2026-08-12 09:21:35 +0800

    💄 Update cart page layout to match store

--- app/pages/list.vue
+++ app/pages/list.vue
@@ -1,104 +1,103 @@
 <template>
-  <main class="flex flex-col grow px-4 laptop:px-12 pb-4">
-    <div class="flex flex-col grow w-full max-w-[1200px] mx-auto">
-      <h1
-        class="mt-4 laptop:mt-10 mb-6 text-highlighted text-xl laptop:text-3xl font-bold"
-        v-text="$t('book_list_title')"
-      />
-      <UCard
-        v-if="!hasLoggedIn"
-        class="w-full max-w-sm mt-8 mx-auto"
-        :ui="{ footer: 'flex justify-end' }"
-      >
-        <p v-text="$t('book_list_please_login')" />
-        <template #footer>
-          <LoginButton />
-        </template>
-      </UCard>
-      <div
-        v-else-if="bookListStore.count > 0"
-        class="relative"
-      >
-        <header class="sticky top-0 z-10 grid grid-cols-12 items-center py-2 text-muted bg-(--app-bg) leading-none border-b border-black/10">
-          <div
-            v-if="!isApp"
-            class="col-span-1"
-          >
-            <UCheckbox
-              :model-value="hasSelectedItems"
-              @update:model-value="handleSelectAllUpdate"
-            />
-          </div>
-          <div
-            :class="[
-              'flex',
-              'items-center',
-              'justify-between',
-              { 'col-span-11': !isApp },
-            ]"
-          >
-            <span v-text="$t('book_list_header_item_label')" />
+  <main class="section-container flex flex-col grow py-4">
+    <h1
+      class="text-highlighted text-xl laptop:text-3xl font-bold"
+      v-text="$t('book_list_title')"
+    />
 
-            <UButton
-              v-if="!isApp"
-              :label="$t('book_list_checkout_button_label')"
-              :loading="isCheckingOut"
-              :disabled="!hasSelectedItems"
-              @click="handleCheckoutButtonClick"
-            />
-          </div>
-        </header>
-
-        <ul class="divide-y divide-black/10">
-          <BookListItem
-            v-for="item in bookListStore.items"
-            :key="item.nftClassId"
-            :nft-class-id="item.nftClassId"
-            :price-index="item.priceIndex"
-            :is-selected="selectedItemIds.has(getBookListItemId(item.nftClassId, item.priceIndex))"
-            @remove="handleBookListItemRemove"
-            @select="handleItemSelect"
-            @unselect="handleItemDeselect"
+    <UCard
+      v-if="!hasLoggedIn"
+      class="w-full max-w-sm mx-auto"
+      :ui="{ footer: 'flex justify-end' }"
+    >
+      <p v-text="$t('book_list_please_login')" />
+      <template #footer>
+        <LoginButton />
+      </template>
+    </UCard>
+    <div
+      v-else-if="bookListStore.count > 0"
+      class="relative"
+    >
+      <header class="sticky top-0 z-10 grid grid-cols-12 items-center py-2 text-muted bg-(--app-bg) leading-none border-b border-black/10">
+        <div
+          v-if="!isApp"
+          class="col-span-1"
+        >
+          <UCheckbox
+            :model-value="hasSelectedItems"
+            @update:model-value="handleSelectAllUpdate"
           />
-        </ul>
-      </div>
-      <div
-        v-else
-        class="flex flex-col items-center grow py-12"
-      >
-        <div class="flex flex-col justify-center items-center gap-2 py-4 grow text-muted">
-          <template v-if="bookListStore.isLoading">
-            <UIcon
-              class="animate-spin"
-              name="i-material-symbols-progress-activity"
-              size="48"
-            />
-            <span
-              class="font-semibold leading-none"
-              v-text="$t('book_list_loading_description')"
-            />
-          </template>
-          <template v-else>
-            <UIcon
-              name="i-material-symbols-shopping-cart-outline-rounded"
-              size="48"
-            />
-            <span
-              class="font-semibold leading-none"
-              v-text="$t('book_list_empty_description')"
-            />
-          </template>
         </div>
+        <div
+          :class="[
+            'flex',
+            'items-center',
+            'justify-between',
+            { 'col-span-11': !isApp },
+          ]"
+        >
+          <span v-text="$t('book_list_header_item_label')" />
+
+          <UButton
+            v-if="!isApp"
+            :label="$t('book_list_checkout_button_label')"
+            :loading="isCheckingOut"
+            :disabled="!hasSelectedItems"
+            @click="handleCheckoutButtonClick"
+          />
+        </div>
+      </header>
+
+      <ul class="divide-y divide-black/10">
+        <BookListItem
+          v-for="item in bookListStore.items"
+          :key="item.nftClassId"
+          :nft-class-id="item.nftClassId"
+          :price-index="item.priceIndex"
+          :is-selected="selectedItemIds.has(getBookListItemId(item.nftClassId, item.priceIndex))"
+          @remove="handleBookListItemRemove"
+          @select="handleItemSelect"
+          @unselect="handleItemDeselect"
+        />
+      </ul>
+    </div>
+    <div
+      v-else
+      class="flex flex-col items-center grow py-12"
+    >
+      <div class="flex flex-col justify-center items-center gap-2 py-4 grow text-muted">
+        <template v-if="bookListStore.isLoading">
+          <UIcon
+            class="animate-spin"
+            name="i-material-symbols-progress-activity"
+            size="48"
+          />
+          <span
+            class="font-semibold leading-none"
+            v-text="$t('book_list_loading_description')"
+          />
+        </template>
+        <template v-else>
+          <UIcon
+            name="i-material-symbols-shopping-cart-outline-rounded"
+            size="48"
+          />
+          <span
+            class="font-semibold leading-none"
+            v-text="$t('book_list_empty_description')"
+          />
+        </template>
         <UButton
           :class="[
-            'max-w-[348px]',
-            'laptop:mt-12',
+            'mt-6 laptop:mt-12',
             { 'opacity-0 pointer-events-none': bookListStore.isLoading },
           ]"
           leading-icon="i-material-symbols-storefront-outline"
           :label="$t('book_list_empty_cta_button_label')"
+          variant="outline"
+          color="neutral"
           size="xl"
-          block
           :to="localeRoute({ name: 'store' })"
         />
       </div>
